eea46f613b7815bf2792b4615acb9f8b282a11e3
minna74 <adam.asseidsiddick.ensao@ump.ac.ma>
2026-04-26 06:36:09 +0100

    feat: ajouter de db_connect.php centralisé a la racine

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$host     = getenv('DB_HOST') ?: 'localhost'; // devient 'mysql' dans Docker
+$dbname   = getenv('DB_NAME') ?: 'gestion_rdv_medical';
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASS') ?: '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
